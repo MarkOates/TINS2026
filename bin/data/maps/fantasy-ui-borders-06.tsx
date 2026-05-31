9faa16aef8e9d1cfafcc44b0fa123cfd6a6e7633
Mark Oates <markoates0@gmail.com>
2026-05-31 04:51:04 -0400

    * Fix camera

---
+++ bin/data/maps/fantasy-ui-borders-06.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="fantasy-ui-borders-06" tilewidth="8" tileheight="8" tilecount="16384" columns="128">
+ <image source="../../../../../Desktop/fantasy-ui-borders-06.png" width="1024" height="1024"/>
+</tileset>
